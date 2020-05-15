183fb847308109b9cf6e7977bb15019659f13a59
RaniOfTheRain <chloecampbell911@gmail.com>
2020-05-15 17:56:58 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,16 @@
+//
+// The config file that contains important information for the bot to run
+// Produced by Chloe Campbell 2020 for the Chip'n'Jam Fundraising Game Jam
+//
+
+module.exports = {
+    discordBotToken = '',
+    
+    commands = {
+
+    },
+
+    emojis = {
+        
+    }
+}
